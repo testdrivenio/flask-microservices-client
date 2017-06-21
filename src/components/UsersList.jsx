13bf8ba6f10a01f29987f687a17af4a0dc531767
Michael Herman <hermanmu@gmail.com>
2017-06-20 21:07:51 -0600

    refactored UsersList component

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 
 const UsersList = (props) => {
   return (
     <div>
-      {
-        props.users.map((user) => {
-          return <h4 key={ user.id } className="well"><strong>{ user.username }</strong> - <em>{user.created_at}</em></h4>
-        })
-      }
+      <h1>All Users</h1>
+      <hr/><br/>
+      <Table striped bordered condensed hover>
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>Email</th>
+            <th>Username</th>
+            <th>Created Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            props.users.map((user) => {
+              return (
+                <tr key={ user.id }>
+                  <td>{ user.id }</td>
+                  <td>{ user.email }</td>
+                  <td>{ user.username }</td>
+                  <td>{ user.created_at }</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </Table>
     </div>
   )
 }
